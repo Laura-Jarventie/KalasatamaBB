d682e65115ea9a2cf5894c0801571e345b52861a
unknown <laurajarventie@hotmail.comcd..git config --global user.email  laurajarventie@hotmail.com>
2023-10-10 19:34:51 +0300

    tunti 10.10

--- card-game/src/App.jsx
+++ card-game/src/App.jsx
@@ -1,50 +1,49 @@
 import './App.css'
+import { useState } from 'react';
 import Card from './Components/Card.jsx'
 
 const getRandomInt = (min, max) =>
 Math.floor(Math.random() * (max - min + 1) ) + min;
 
-console.log(getRandomInt(1,2))
-
-  const pelaajanKortti = {
-    kuva: 'http://placekitten.com/120/100?image=' + getRandomInt(1,16),
+  const kortti = (index) => ({
+    kuva: 'http://placekitten.com/120/100?image=' + index,
     stats: [
       {name: 'paino', value: getRandomInt(3,15)},
       {name: 'söpöys', value: getRandomInt(1,100)},
       {name: 'nopeus', value: getRandomInt(1,40)},
       {name: 'ruokahalu', value: getRandomInt(1,5)},
     ],
-  };
+  });
 
-  const vastustajanKortti = {
-    kuva: 'http://placekitten.com/120/100?image=' + getRandomInt(1,16),
-    stats: [
-      {name: 'paino', value: getRandomInt(3,15)},
-      {name: 'söpöys', value: getRandomInt(1,100)},
-      {name: 'nopeus', value: getRandomInt(1,40)},
-      {name: 'ruokahalu', value: getRandomInt(1,5)},
-    ],
-  };
+  const korttipakka = Array(16).fill(null).map((_,index)=>kortti(index));
+  console.log(korttipakka);
 
-  function App() {
+  const pelaajanKortti = korttipakka[0];
+  const vastustajanKortti = korttipakka[1];
+
+
+function App() {
+
+const [result, setResult] = useState('');
+
 
 function vertaaKortteja() {
  const playerStat = pelaajanKortti.stats[0];
  const opponentStat = vastustajanKortti.stats[0];
 
- let result= '';
+ if (playerStat.value === opponentStat.value) setResult('tasapeli');
+ else if (playerStat.value > opponentStat.value) setResult('voitto');
+ else setResult('häviö');
 
- if (playerStat.value === opponentStat.value) result = 'tasapeli';
- else if (playerStat.value > opponentStat.value) result = 'voitto';
- else result = 'häviö'
-
- console.log(result);
+ //console.log(result);
 
 }
 
 
   return (
     <>
+    <h1>KISSAKORTTIPELI</h1>
+    <p>{result}</p>
     <p>oma kortti</p>
       <Card card={pelaajanKortti}/>
       <button onClick={vertaaKortteja} type='button'>Pelaa</button>
